ba8add3b0d784903220ca1bdce32b1b4cffcec69
palasmert <134235672+palasmert@users.noreply.github.com>
2024-03-21 00:23:29 +0300

    animation

---
+++ Africa-Appl/Africa/Africa/View/MapAnnotationView.swift
@@ -0,0 +1,47 @@
+//
+//  MapAnnotationView.swift
+//  Africa
+//
+//  Created by mert palas on 21.03.2024.
+//
+
+import SwiftUI
+
+struct MapAnnotationView: View {
+    //MARK: - PROP
+    var location : nationalParkLocation
+    @State private var animation: Double = 0.0
+    //MARK: - BODY
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color.accentColor)
+                .frame(width: 54, height: 54, alignment: .center)
+            Circle()
+                .stroke(Color.accentColor, lineWidth: 2)
+                .frame(width: 52, height: 52, alignment: .center)
+                .scaleEffect(1 + CGFloat(animation))
+                .opacity(1 - animation)
+            Image(location.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 48, height: 48, alignment: .center)
+                .clipShape(Circle())
+        }//Zstack
+        .onAppear() {
+            withAnimation(Animation.easeOut(duration: 2).repeatForever(autoreverses: false)) {
+                animation = 1
+            }
+        }
+    }
+}
+
+struct MapAnnotationView_Previews: PreviewProvider{
+    static var locations: [nationalParkLocation] = Bundle.main.decode("locations.json")
+    
+    static var previews: some View {
+        MapAnnotationView(location: locations[0])
+            .previewLayout(.sizeThatFits)
+            .padding(/*@START_MENU_TOKEN@*/EdgeInsets()/*@END_MENU_TOKEN@*/)
+    }
+}
